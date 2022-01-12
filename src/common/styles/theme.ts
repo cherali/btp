0103dd5ddc8de0cbd5c4798c516c4ca72f7792f6
alich6 <alicheraghi148@gmail.com>
2022-01-12 19:45:05 +0330

    add theme

--- src/common/styles/theme.ts
+++ src/common/styles/theme.ts
@@ -1,3 +1,19 @@
+declare module 'styled-components' {
+	export interface DefaultTheme {
+		colors: IColors;
+		fontSize: IFontSize;
+	}
+}
+
+
+interface IColors {
+	white: string;
+	black: string;
+	primary: string;
+	secondary: string;
+	yellow: string;
+}
+
 interface IFontSize {
 	xxSmall: string; // 3x small
 	xSmall: string; // 2x small
@@ -10,8 +26,17 @@ interface IFontSize {
 }
 
 interface ITheme {
+	colors: IColors;
 	fontSize: IFontSize;
 }
+
+const whiteColor: string = '#fff'
+const blackColor: string = '#000'
+const priamryColor: string = '#262626'
+const secondaryColor = '#409581'
+const yellowColor = '#f7a80b'
+
+
 
 const fontSize: IFontSize = {
 	xxSmall: '10px', // 3x small
@@ -27,5 +52,12 @@ const fontSize: IFontSize = {
 
 
 export const theme: ITheme = {
+	colors: {
+		white: whiteColor,
+		black: blackColor,
+		primary: priamryColor,
+		secondary: secondaryColor,
+		yellow: yellowColor,
+	},
 	fontSize,
 }
